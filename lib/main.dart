5090a3f9712d95bdb629ddd0b83dc9dc6e19518d
MuchAidilPurnama <aidilwind422@gmail.com>
2022-03-18 11:03:05 +0700

    json

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:parsing_ujikom/sample_json.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:parsing_ujikom/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyHomePage(),
+      home: ProfilePage(),
     );
   }
 }
@@ -35,8 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
       _jsonContent = sample.toString();
     });
   }
+
   late String _jsonContent = "";
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Container(
                       height: 50,
                       width: 150,
-                      color: Colors.grey,
+                      color: Colors.blue,
                       alignment: Alignment.center,
                       child: const Text("Read JSON File"),
                     ),
